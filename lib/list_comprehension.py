e15955359d3d01d56773d979443d3c6e21743dc0
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-07 13:44:58 -0500

    write solutions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    return [i for i in num_list if i % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [str + "!" for str in sentence_list]
